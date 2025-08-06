db690e6b7fc2f222ff49d82401e5daabcc669428
Rutul5440 <rutulsuthar2018@gmail.com>
2025-08-06 18:16:20 +0530

    fixed class

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,6 +1,12 @@
+@props([
+    'label' => '',
+    'name',
+    'class' => 'form-select',
+])
+
 <div class="mb-3">
     <label class="form-label">{{ $label }}</label>
-    <select name="{{ $name }}" class="form-select">
+    <select name="{{ $name }}" {{ $attributes->merge(['class' => $class]) }}>
         <option value="">-- Select --</option>
         <option value="India">Test 1</option>
         <option value="USA">Test 2</option>
